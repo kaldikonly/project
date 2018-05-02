bd6165594e39967873c9d4280dcc86d9d181e5f4
ColliderBang <collide.ua@gmail.com>
2018-05-02 18:27:27 +0300

    Revert "test"
    This reverts commit 5727930dceabaf179c45a8f2d15d373663ccf711.

---
+++ client/src/components/urls/Word_news.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './word_news.css';
+
+class Word_news extends Component{
+	render(){
+		return (
+<div className="col word_b">
+	<div className="word">
+		<div className="block3">
+			<span className="bl_txt">Олимпиада</span>
+			<span className="bl_count">19856</span>
+		</div>
+
+		<div className="block2">
+			<span className="bl_txt">Медаль</span>
+			<span className="bl_count">19256</span>
+		</div>
+
+			<div className="block2">
+			<span className="bl_txt">Золото</span>
+			<span className="bl_count">19056</span>
+		</div>
+
+			<div className="block2">
+			<span className="bl_txt">Серебро</span>
+			<span className="bl_count">18726</span>
+		</div>
+
+		<div className="block4">
+			<span className="bl_txt">Летняя</span>
+			<span className="bl_count">15776</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Зиняяя</span>
+			<span className="bl_count">14570</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Спорт</span>
+			<span className="bl_count">12563</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Сортсмены</span>
+			<span className="bl_count">12009</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Наши</span>
+			<span className="bl_count">11246</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Победители</span>
+			<span className="bl_count">10216</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Призеры</span>
+			<span className="bl_count">9856</span>
+		</div>
+
+		</div>
+	</div>
+
+
+	);
+	}
+}
+export default Word_news;
