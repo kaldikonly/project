971eda25d46eeb407b24ac6afd2871e7e099bd8b
ColliderBang <collide.ua@gmail.com>
2018-05-11 14:28:06 +0300

    Блок 12 важных слов
    Переписал заново html и css

--- client/src/components/urls/Word_news.js
+++ client/src/components/urls/Word_news.js
@@ -5,7 +5,7 @@ class Word_news extends Component{
 	render(){
 		return (
 
-<div className="col word_b">
+<div className="col word_b shadow-sm p-3 mb-5 bg-white rounded">
 	<div className="word">
 		<div className="block3">
 			<span className="bl_txt">Олимпиада</span>
@@ -18,54 +18,194 @@ class Word_news extends Component{
 			<span className="bl_count">19256</span>
 		</div>
 
-			<div className="block2">
-			<span className="bl_txt">Золото</span>
-			<span className="bl_count">19056</span>
-		</div>
-
-			<div className="block2">
-			<span className="bl_txt">Серебро</span>
-			<span className="bl_count">18726</span>
-		</div>
 
 		<div className="block4">
 			<span className="bl_txt">Летняя</span>
 			<span className="bl_count">15776</span>
 			</div>
 
-		<div className="block4">
-			<span className="bl_txt">Зиняяя</span>
-			<span className="bl_count">14570</span>
-			</div>
 
-		<div className="block4">
-			<span className="bl_txt">Спорт</span>
-			<span className="bl_count">12563</span>
-			</div>
-
-		<div className="block4">
-			<span className="bl_txt">Сортсмены</span>
-			<span className="bl_count">12009</span>
-			</div>
 
 		<div className="block5">
 			<span className="bl_txt">Наши</span>
 			<span className="bl_count">11246</span>
 			</div>
 
-		<div className="block5">
-			<span className="bl_txt">Победители</span>
-			<span className="bl_count">10216</span>
-			</div>
 
-		<div className="block5">
-			<span className="bl_txt">Призеры</span>
-			<span className="bl_count">9856</span>
-		</div>
+
+
+            <div className="col ns_important_words" id="block_important_words">
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">Олимпиада</span>
+                        <span className="important_word_quantity">19856</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                         aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">серебро</span>
+                        <span className="important_word_quantity">19 553</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">соревнование</span>
+                        <span className="important_word_quantity">18 000</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">победа</span>
+                        <span className="important_word_quantity">17 800</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">Россия</span>
+                        <span className="important_word_quantity">17 630</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">Украина</span>
+                        <span className="important_word_quantity">17 000</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">бобслей</span>
+                        <span className="important_word_quantity">16 300</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">биатлон</span>
+                        <span className="important_word_quantity">15 600</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">зимняя</span>
+                        <span className="important_word_quantity">15 000</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">игры</span>
+                        <span className="important_word_quantity">14 854</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">Пхенчхан</span>
+                        <span className="important_word_quantity">14 520</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">допинг</span>
+                        <span className="important_word_quantity">13 241</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div className="important_word">
+                    <div className="important_word_container">
+                        <span className="important_word_name">бронза</span>
+                        <span className="important_word_quantity">13 000</span>
+                    </div>
+
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+            </div>
 
 		</div>
 	</div>
 
+    // // <div className="col ns_important_words" id="block_important_words">
+    // //      <div className="important_word">
+    //             <span className="important_word_name">Призеры</span>
+    //             <span className="important_word_quantity">9856</span>
+    //             <div className="progress">
+    //                 <div className="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0"
+    //     aria-valuemax="100"></div>
+    //             </div>
+    //         </div>
+    // // </div>
 
 	);
 	}
