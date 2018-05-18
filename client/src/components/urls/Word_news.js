9e0208284a8ce88d45df87c2cd41d8f0979140d7
ColliderBang <collide.ua@gmail.com>
2018-05-18 15:28:12 +0300

    test 2

--- client/src/components/urls/Word_news.js
+++ client/src/components/urls/Word_news.js
@@ -5,6 +5,7 @@ class Word_news extends Component{
 	render(){
 		return (
 
+<<<<<<< HEAD
 
             <div className="container shadow-sm ns_important_words" id="block_important_words">
                 <div className="header_block_important_words">
@@ -170,6 +171,69 @@ class Word_news extends Component{
 
             </div>
 
+=======
+<div className="col word_b">
+	<div className="word">
+		<div className="block3">
+			<span className="bl_txt">Олимпиада</span>
+			<span className="bl_count">19856</span>
+		</div>
+
+
+		<div className="block2">
+			<span className="bl_txt">Медаль</span>
+			<span className="bl_count">19256</span>
+		</div>
+
+			<div className="block2">
+			<span className="bl_txt">Золото</span>
+			<span className="bl_count">19056</span>
+		</div>
+
+			<div className="block2">
+			<span className="bl_txt">Серебро</span>
+			<span className="bl_count">18726</span>
+		</div>
+
+		<div className="block4">
+			<span className="bl_txt">Летняя</span>
+			<span className="bl_count">15776</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Зиняяя</span>
+			<span className="bl_count">14570</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Спорт</span>
+			<span className="bl_count">12563</span>
+			</div>
+
+		<div className="block4">
+			<span className="bl_txt">Сортсмены</span>
+			<span className="bl_count">12009</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Наши</span>
+			<span className="bl_count">11246</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Победители</span>
+			<span className="bl_count">10216</span>
+			</div>
+
+		<div className="block5">
+			<span className="bl_txt">Призеры</span>
+			<span className="bl_count">9856</span>
+		</div>
+
+		</div>
+	</div>
+
+>>>>>>> parent of 971eda2... Блок 12 важных слов
 
 	);
 	}
